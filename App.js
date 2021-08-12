dc6faeaeaa76a42239cb07d220fa32ab5113de0e
hoanghoan1o9 <you@example.comhoang.hoan.17.10.1999@gmail.com>
2021-08-12 15:55:59 +0700

    working with list

--- App.js
+++ App.js
@@ -6,116 +6,88 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  SectionList,
+ } from 'react-native';
 
 const App = () => {
 
+  const [Sections, setSections] = useState([
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2'],
+    },
+  ]);
+  const onRefresh = () => {
+    setRefreshing(true);
+    const adding_index = Sections.length + 1;
+    setSections([...Sections,
+    {
+      title: 'Title ' + adding_index,
+      data:
+        [
+          'Item ' + adding_index + '-1',
+          'Item ' + adding_index + '-2',
+        ],
+    },
+    ]);
+    setRefreshing(false);
+  };
+  const [Refreshing, setRefreshing] = useState(false);
 
   return (
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <View style={styles.text1}>
-          <Text>1</Text>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={Sections}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text_item}>{item}</Text>
         </View>
-        <View style={styles.text2}>
-          <Text>2</Text>
+      )}
+      renderSectionHeader={({ section }) => (
+        <View style={styles.header}>
+          <Text style={styles.text_header}>{section.title}</Text>
         </View>
-        <View style={styles.text3}>
-          <Text>3</Text>
-        </View>
-      </View>
-      <View style={styles.view2}>
-        <View style={styles.text4}>
-          <Text>4</Text>
-        </View>
-        <View style={styles.text5}>
-          <Text>5</Text>
-        </View>
-      </View>
-      <View style={styles.view3}>
-        <View style={styles.text6}>
-          <Text>6</Text>
-        </View>
-        <View style={styles.text7}>
-          <Text>7</Text>
-        </View>
-      </View>
-    </View>
+      )}
+      refreshControl={
+        < RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    flex:1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
-  },
-  view1: {
-    flex:1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
+  header: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  view2: {
-    flex:2,
-    flexDirection: 'column',
-    alignItems: 'stretch',
-  },
-  view3: {
-    flex: 4,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-
-  text1: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: '#03ffff',
+    borderWidth: 2,
   },
-  text2: {
-    flex: 2,
-    alignItems: 'center',
+  item: {
+    borderBottomWidth: 1,
     justifyContent: 'center',
-    color: 'black',
-    backgroundColor: '#ff00ff',
+    alignItems: 'center',
   },
-  text3: {
-    flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: '#ffff02',
+  text_header: {
+    color: '#000000',
+    fontSize: 45,
+    fontStyle: 'italic',
+    margin: 10,
   },
-  text4: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: 'red',
-  },
-  text5: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: 'green',
-  },
-  text6: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: 'white',
-  },
-  text7: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    backgroundColor: 'blue',
+  text_item: {
+    color: '#000000',
+    fontSize: 35,
+    margin: 5,
   },
 });
 
